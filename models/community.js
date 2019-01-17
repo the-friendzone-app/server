caf700a3dc328643bc5332726915e51ed04542c9
talexcrowell <talexcrowell@gmail.com>
2019-01-16 21:23:04 -0800

    added userscore model

--- models/community.js
+++ models/community.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const userCommentsSchema = new mongoose.Schema(
   {
     comment: {type: String, required: true},
-    hashedUsername: {type: String, required: true}
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+    post: 
   }
 );
 
---
+++ models/userscore.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema(
+  {
+    user:{type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    friendScore: {type: Number, default: 0},
+    strikeScore: {type: Number, default: 0},
+    aggroScore: {type: Number, default: 0}
+  }
+);
+
+schema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, res) =>{
+    delete res._id;
+    delete res.__v;
+  }
+});
+
+module.exports = mongoose.model('Userscore', schema);
