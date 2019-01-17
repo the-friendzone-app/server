78fc93cdbf5c082f1b051b9852c8daf52a754740
talexcrowell <talexcrowell@gmail.com>
2019-01-16 21:29:58 -0800

    added useranswer model

---
+++ models/useranswer.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema(
+  {
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    answer: {type: String},
+    question: {type: mongoose.Schema.Types.ObjectId, ref: 'Question'}
+  }
+);
+
+schema.set('timestamps', true);
+
+schema.set('toJSON',{
+  virtuals: true,
+  transform: (doc, res) =>{
+    delete res._id;
+    delete res.__v;
+  }
+});
+
+module.exports = mongoose.model('Useranswer', schema);
