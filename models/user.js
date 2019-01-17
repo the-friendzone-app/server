1fd8ab5df4193c1ceb5e5889e4188ac7e15812b3
ThomasChun <tchun2055@gmail.com>
2019-01-17 07:00:12 -1000

    login and auth

--- models/user.js
+++ models/user.js
@@ -1,65 +1,100 @@
-'use strict';
+'use strict'; 
 
-const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose'); 
 
-const schema = new mongoose.Schema(
-  {
-    username: {type: String, required: true, unique: true},
-    hashedUsername: {type: String},
-    password: {type: String, required: true},
-    userVerificationCode: {type: String, required: true},
-    verified: {type: Boolean, default: false},
-    introQuizCompleted: {type: Boolean, default: false},
-    location: {type: String, default: ''},
-    lists:{
-      suggestedFriendsList: [{type: Object}],
-      friendsList: [{type: Object}],
-      ignoreList:[{type: Object}],
-      meetupJoinedList: [{type: Object}],
-      meetupCreatedList: [{type: Object}],
-    },
-    scores: {
-      friendScore: {type: Number, default: 0},
-      strikeScore: {type: Number, default: 0},
-      aggroScore: {type: Number, default: 0},
-      meetupCreateScore: {type: Number, default: 0},
-      meetupJoinScore: {type: Number, default: 0},
-      pollScore: {type: Number, default: 0},
-      communityScore: {type: Number, default: 0},
-    },
-    profile: {
-      selfTalker: {type: Boolean},
-      selfListener: {type: Boolean},
-      preferenceTalker: {type: Boolean},
-      preferenceListener: {type: Boolean},
-    }
-  }
-);
 
-schema.set('timestamps', true);
+const schema = new mongoose.Schema({ 
+  firstName: { type: String, default: '' },
+  lastName: { type: String, default: '' }, 
+  username: { type: String, required: true, unique: true }, 
+  password: { type: String, required: true}
+}); 
 
-schema.set('toJSON', {
-  virtuals: true,
-  transform: (doc, res) => {
-    delete res._id;
-    delete res.hashedUsername;
-    delete res.location;
-    delete res.__v;
-    delete res.password;
-    delete res.introQuizCompleted;
-    delete res.lists;
-    delete res.scores;
-    delete res.profile;
+schema.set('toJSON', { 
+  virtual: true, 
+  transform: (doc, result) => { 
+    delete result._id; 
+    delete result.__v; 
+    delete result.password; 
   }
 });
 
-schema.methods.validatePassword = function(password){
-  return bcrypt.compare(password, this.password);
-};
+schema.methods.validatePassword = function (pwd) { 
+  const currentUser = this; 
+  return bcrypt.compare(pwd, currentUser.password); 
+}; 
 
-schema.statics.hashPassword = function(password){
-  return bcrypt.hash(password, 10);
-};
+schema.statics.hashPassword = function (pwd) { 
+  return bcrypt.hash(pwd, 10); 
+}; 
 
-module.exports = mongoose.model('User', schema);
+module.exports = mongoose.model('User', schema);  
+
+
+
+// 'use strict';
+
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
+
+// const schema = new mongoose.Schema(
+//   {
+//     username: {type: String, required: true, unique: true},
+//     hashedUsername: {type: String},
+//     password: {type: String, required: true},
+//     userVerificationCode: {type: String, required: true},
+//     verified: {type: Boolean, default: false},
+//     introQuizCompleted: {type: Boolean, default: false},
+//     location: {type: String, default: ''},
+//     lists:{
+//       suggestedFriendsList: [{type: Object}],
+//       friendsList: [{type: Object}],
+//       ignoreList:[{type: Object}],
+//       meetupJoinedList: [{type: Object}],
+//       meetupCreatedList: [{type: Object}],
+//     },
+//     scores: {
+//       friendScore: {type: Number, default: 0},
+//       strikeScore: {type: Number, default: 0},
+//       aggroScore: {type: Number, default: 0},
+//       meetupCreateScore: {type: Number, default: 0},
+//       meetupJoinScore: {type: Number, default: 0},
+//       pollScore: {type: Number, default: 0},
+//       communityScore: {type: Number, default: 0},
+//     },
+//     profile: {
+//       selfTalker: {type: Boolean},
+//       selfListener: {type: Boolean},
+//       preferenceTalker: {type: Boolean},
+//       preferenceListener: {type: Boolean},
+//     }
+//   }
+// );
+
+// schema.set('timestamps', true);
+
+// schema.set('toJSON', {
+//   virtuals: true,
+//   transform: (doc, res) => {
+//     delete res._id;
+//     delete res.hashedUsername;
+//     delete res.location;
+//     delete res.__v;
+//     delete res.password;
+//     delete res.introQuizCompleted;
+//     delete res.lists;
+//     delete res.scores;
+//     delete res.profile;
+//   }
+// });
+
+// schema.methods.validatePassword = function(password){
+//   return bcrypt.compare(password, this.password);
+// };
+
+// schema.statics.hashPassword = function(password){
+//   return bcrypt.hash(password, 10);
+// };
+
+// module.exports = mongoose.model('User', schema);
---
+++ routes/user.js
@@ -0,0 +1,105 @@
+'use strict';
+
+const express = require('express');
+
+const User = require('../models/user');
+
+const router = express.Router();
+
+function validateNewUser(req, res, next) {
+  const { username, password } = req.body;
+
+  let err;
+  if (!username) {
+    err = new Error('Username is required');
+    err.location = 'username';
+    err.code = 400;
+  } else if (!password) {
+    err = new Error('Password is required');
+    err.location = 'password';
+    err.code = 400;
+  } else if (username.length < 1) {
+    err = new Error('Username must be at least one character long');
+    err.location = 'password';
+    err.code = 422;
+  } else if (password.length < 10 || password.length > 72) {
+    err = new Error('Password must be between 10 and 72 characters long');
+    err.location = 'password';
+    err.code = 422;
+  } else if(username.trim() !== username) {
+    err = new Error('Username must not have leading/trailing whitespace');
+    err.location = 'username';
+    err.code = 422;
+  } else if (password.trim() !== password) {
+    err = new Error('Password must not have leading/trailing whitespace');
+    err.location = 'password';
+    err.code = 422;
+  }
+
+  if (err) {
+    err.reason = 'ValidationError'; // For reduxForm
+    next(err);
+    return;
+  }
+
+  next();
+}
+
+router.post('/', validateNewUser, (req, res, next) => {
+
+  const { username, password, firstName, lastName } = req.body;
+
+  User.hashPassword(password)
+    .then((digest) => User.create({ username, firstName, lastName, password: digest }))
+    .then((user) => {
+      res
+        .status(201)
+        .location(`${req.baseUrl}/${user._id}`)
+        .json(user);
+    })
+    .catch((err) => {
+      if (err.code === 11000 && err.name === 'MongoError') {
+        // Username already exists
+        const err = new Error('Username already taken');
+        err.location = 'username';
+        err.code = 422;
+        err.reason = 'ValidationError';
+
+        return Promise.reject(err);
+      }
+    })
+    .catch(next);
+});
+
+module.exports = router;
+
+// 'use strict';
+
+// const express = require('express');
+
+// const User = require('../models/user');
+
+// const router = express.Router();
+
+// router.post('/', (req, res, next) => {
+//   const { username, password, firstName, lastName } = req.body;
+
+//   User.hashPassword(password)
+//     .then((digest) => User.create({ username, firstName, lastName, password: digest }))
+//     .then((result) => {
+//       res
+//         .status(201)
+//         .location(`${req.baseUrl}/${result._id}`)
+//         .json(result);
+//     })
+//     .catch((err) => {
+//       if (err.code === 11000 && err.name === 'MongoError') {
+//         // Username already exists
+//         err.status = 400;
+//         return Promise.reject(err);
+//       }
+//     })
+//     .catch(next);
+// });
+
+// module.exports = router;
